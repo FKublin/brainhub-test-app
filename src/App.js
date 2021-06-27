6442afb57ac5587c442524c709941281ac8fc775
FKublin <fpkublin@gmail.com>
2021-06-27 10:48:25 +0200

    Styling and component integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 import logo from './logo.svg';
 import './App.css';
+import EventForm from './components/EventForm'
 
-function App() {
+
+const App = () => {
+
+  const handleSignIn = values => {
+    console.log(values);
+    fetch('http://localhost:4000/events', {
+      method: 'POST',
+          headers: {
+           Accept: 'application/json',
+           'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({
+           firstName: values.firstName,
+           lastName: values.lastName,
+           email: values.email,
+           eventDate: new Date('2023-03-02')
+         }),
+    }).then(res => res.json())
+    .then(res => console.log(res))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+        <p className="App-intro">
+          Contact Form
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <EventForm onSubmit={handleSignIn} />
+      </div>
     </div>
   );
+
+
 }
 
 export default App;
